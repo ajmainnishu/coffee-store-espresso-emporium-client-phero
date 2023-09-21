853840775b9eb7a49e029a96bfd9c6678ac1c1fd
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-21 17:26:17 +0600

    feat: Logo component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Logo from './shared/Logo';
 
 function App() {
   return (
     <div>
+      <Logo></Logo>
     </div>
   )
 }
 
-export default App
+export default App;

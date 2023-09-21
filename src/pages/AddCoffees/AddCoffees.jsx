45d41ce7d7f974f2e0e58bb31d312ca1a67b01ce
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-22 01:58:23 +0600

    feat: AddCoffees data insert added

--- src/pages/AddCoffees/AddCoffees.jsx
+++ src/pages/AddCoffees/AddCoffees.jsx
@@ -1,7 +1,39 @@
 import { Link } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const AddCoffees = () => {
+    const handleAddCoffee = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const chef = form.chef.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+        const price = form.price.value;
+        const user = { name, chef, supplier, taste, category, details, photo, price };
+        fetch('http://localhost:5000/coffees', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    Swal.fire(
+                        'Add Coffee',
+                        'Your added coffess data successfully',
+                        'success'
+                    )
+                    form.reset()
+                }
+            })
+    }
     return (
         <div className='addcoffee-b bg-[url("./src/assets/images/addCoffee/addcoffee-bg.png")] bg-contain bg-no-repeat'>
             <div className="w-4/6 mx-auto pb-32 pt-12">
@@ -9,7 +41,7 @@ const AddCoffees = () => {
                 <div className="py-16 px-28 bg-[#F4F3F0] rounded-md">
                     <h2 className="text-[#374151] font-rancho text-5xl drop-shadow-lg text-center">Add New Coffee</h2>
                     <p className="text-lg text-center text-[#1B1A1A] opacity-70 px-20 py-8">It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
-                    <form>
+                    <form onSubmit={handleAddCoffee}>
                         <div className="flex gap-x-6 items-center mb-6">
                             <div className="form-control w-full">
                                 <label className="pb-4">

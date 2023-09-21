41fa6988b144694976e7c8320b07e0f84e5a4d37
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-21 22:26:26 +0600

    feat: UpdateCoffees component added

--- src/pages/UpdateCoffees/UpdateCoffees.jsx
+++ src/pages/UpdateCoffees/UpdateCoffees.jsx
@@ -1,16 +1,16 @@
 import { Link } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
 
-const AddCoffees = () => {
+const UpdateCoffees = () => {
     return (
         <div className='addcoffee-b bg-[url("./src/assets/images/addCoffee/addcoffee-bg.png")] bg-contain bg-no-repeat'>
             <div className="w-4/6 mx-auto pb-32 pt-12">
                 <Link to='/' className="text-[#374151] font-rancho text-3xl mb-12 drop-shadow-lg flex items-center gap-x-4"><FaArrowLeft className='text-xl' />Back To home</Link>
                 <div className="py-16 px-28 bg-[#F4F3F0] rounded-md">
-                    <h2 className="text-[#374151] font-rancho text-5xl drop-shadow-lg text-center">Add New Coffee</h2>
+                    <h2 className="text-[#374151] font-rancho text-5xl drop-shadow-lg text-center">Update Existing Coffee Details</h2>
                     <p className="text-lg text-center text-[#1B1A1A] opacity-70 px-20 py-8">It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
                     <form>
-                        <div className="flex gap-x-6 items-center mb-6">
+                    <div className="flex gap-x-6 items-center mb-6">
                             <div className="form-control w-full">
                                 <label className="pb-4">
                                     <span className="text-xl font-semibold opacity-80">Name</span>
@@ -67,7 +67,7 @@ const AddCoffees = () => {
                             </div>
                         </div>
                         <div className="text-center">
-                            <button className="font-rancho text-[#331A15] text-2xl w-full py-3.5 border-2 border-[#331A15] rounded-md bg-[#D2B48C]">Add Coffee</button>
+                            <button className="font-rancho text-[#331A15] text-2xl w-full py-3.5 border-2 border-[#331A15] rounded-md bg-[#D2B48C]">Update Coffee Details</button>
                         </div>
                     </form>
                 </div>
@@ -76,4 +76,4 @@ const AddCoffees = () => {
     );
 };
 
-export default AddCoffees;
+export default UpdateCoffees;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home/Home";
 import AddCoffees from "../pages/AddCoffees/AddCoffees";
+import UpdateCoffees from "../pages/UpdateCoffees/UpdateCoffees";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
             {
                 path: 'add-coffees',
                 element: <AddCoffees />
+            },
+            {
+                path: 'update-coffees',
+                element: <UpdateCoffees />
             }
         ]
     }
